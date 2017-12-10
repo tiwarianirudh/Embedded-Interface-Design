bb98fec3e6f083e20481a5d026d67ab0cffcfba4
tiwarianirudh <anirudh.tiwari@colorado.edu>
2017-12-09 19:59:30 -0700

    Adding buttons to UI

--- Comm_Protocol_Comparison/client/client.py
+++ Comm_Protocol_Comparison/client/client.py
@@ -19,9 +19,7 @@ import ast
 import sys
 
 
-
 class Ui_MainWindow(object):
-
     def __init__(self):
         self.sqs = boto3.resource('sqs')
         # Call the queue by name
@@ -37,54 +35,71 @@ class Ui_MainWindow(object):
         self.num_readings = 0
         self.mult_factor = 1.0
         self.add_factor = 0.0
-        self.unit = " C\n"
+        self.unit = ' \u00b0' + " C\n"
         self.final_mesg = ""
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1316, 671)
+        MainWindow.resize(1100, 671)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.RequestData = QtWidgets.QPushButton(self.centralWidget)
-        self.RequestData.setGeometry(QtCore.QRect(330, 200, 181, 121))
+        self.RequestData.setGeometry(QtCore.QRect(310, 150, 181, 71))
         self.RequestData.setAutoFillBackground(False)
         self.RequestData.setObjectName("RequestData")
         self.FahrenheitToCelcius = QtWidgets.QRadioButton(self.centralWidget)
         self.FahrenheitToCelcius.setGeometry(QtCore.QRect(60, 210, 70, 27))
         self.FahrenheitToCelcius.setObjectName("FahrenheitToCelcius")
         self.CtoFlabel = QtWidgets.QLabel(self.centralWidget)
-        self.CtoFlabel.setGeometry(QtCore.QRect(30, 310, 210, 21))
+        self.CtoFlabel.setGeometry(QtCore.QRect(30, 310, 221, 21))
+        self.CtoFlabel.setAlignment(QtCore.Qt.AlignCenter)
         self.CtoFlabel.setObjectName("CtoFlabel")
         self.CelciusToFahrenhite = QtWidgets.QRadioButton(self.centralWidget)
-        self.CelciusToFahrenhite.setGeometry(QtCore.QRect(60, 350, 75, 27))
-        self.CelciusToFahrenhite.setObjectName("CelsiusToFahrenheit")
+        self.CelciusToFahrenhite.setGeometry(QtCore.QRect(60, 350, 66, 27))
+        self.CelciusToFahrenhite.setObjectName("CelciusToFahrenhite")
         self.FtoClabel = QtWidgets.QLabel(self.centralWidget)
-        self.FtoClabel.setGeometry(QtCore.QRect(30, 180, 210, 21))
+        self.FtoClabel.setGeometry(QtCore.QRect(30, 180, 191, 21))
+        self.FtoClabel.setAlignment(QtCore.Qt.AlignCenter)
         self.FtoClabel.setObjectName("FtoClabel")
         self.label = QtWidgets.QLabel(self.centralWidget)
         self.label.setGeometry(QtCore.QRect(300, 0, 211, 131))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.label.setFont(font)
         self.label.setAutoFillBackground(True)
-        self.label.setTextFormat(QtCore.Qt.RichText)
+        self.label.setTextFormat(QtCore.Qt.AutoText)
         self.label.setScaledContents(True)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setWordWrap(True)
         self.label.setIndent(35)
         self.label.setObjectName("label")
-        # Message Box for displaying data/error messages
         self.MessageBox = QtWidgets.QTextEdit(self.centralWidget)
-        self.MessageBox.setGeometry(QtCore.QRect(760, 0, 531, 601))
+        self.MessageBox.setGeometry(QtCore.QRect(670, 10, 411, 601))
         self.MessageBox.setObjectName("MessageBox")
         self.label_2 = QtWidgets.QLabel(self.centralWidget)
-        self.label_2.setGeometry(QtCore.QRect(630, 260, 67, 21))
+        self.label_2.setGeometry(QtCore.QRect(550, 260, 81, 21))
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
         self.ClearMessage = QtWidgets.QPushButton(self.centralWidget)
-        self.ClearMessage.setGeometry(QtCore.QRect(600, 520, 151, 29))
+        self.ClearMessage.setGeometry(QtCore.QRect(510, 520, 151, 29))
         self.ClearMessage.setObjectName("ClearMessage")
         self.Close = QtWidgets.QPushButton(self.centralWidget)
-        self.Close.setGeometry(QtCore.QRect(210, 520, 121, 31))
+        self.Close.setGeometry(QtCore.QRect(150, 520, 121, 31))
         self.Close.setObjectName("Close")
+        self.RequestData_2 = QtWidgets.QPushButton(self.centralWidget)
+        self.RequestData_2.setGeometry(QtCore.QRect(310, 240, 181, 71))
+        self.RequestData_2.setAutoFillBackground(False)
+        self.RequestData_2.setObjectName("RequestData_2")
+        self.RequestData_3 = QtWidgets.QPushButton(self.centralWidget)
+        self.RequestData_3.setGeometry(QtCore.QRect(310, 340, 181, 71))
+        self.RequestData_3.setAutoFillBackground(False)
+        self.RequestData_3.setObjectName("RequestData_3")
         MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1316, 26))
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1100, 23))
         self.menuBar.setObjectName("menuBar")
         MainWindow.setMenuBar(self.menuBar)
         self.mainToolBar = QtWidgets.QToolBar(MainWindow)
@@ -98,29 +113,30 @@ class Ui_MainWindow(object):
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
 
         self.retranslateUi(MainWindow)
-        # Calling appropriate functions on different button clicks
-        self.Close.clicked.connect(MainWindow.close)
         self.ClearMessage.clicked.connect(self.MessageBox.clear)
+        self.Close.clicked.connect(MainWindow.close)
         self.RequestData.clicked.connect(self.fetch_data)
+        self.RequestData_2.clicked.connect(self.plotGraph)
+        self.RequestData_3.clicked.connect(self.test_protocols)
         self.FahrenheitToCelcius.clicked.connect(self.fah_to_cel)
         self.CelciusToFahrenhite.clicked.connect(self.cel_to_fah)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Weather"))
         self.RequestData.setText(_translate("MainWindow", "Request Data"))
-        self.FahrenheitToCelcius.setText(_translate("MainWindow", "F to  C"))
+        self.FahrenheitToCelcius.setText(_translate("MainWindow", ' \u00b0' + "C"))
         self.CtoFlabel.setText(_translate("MainWindow", "Change Scale to Fahrenheit"))
-        self.CelciusToFahrenhite.setText(_translate("MainWindow", "C to F"))
+        self.CelciusToFahrenhite.setText(_translate("MainWindow", ' \u00b0' + "F"))
         self.FtoClabel.setText(_translate("MainWindow", "Change Scale to Celsius"))
         self.label.setText(_translate("MainWindow", "Weather Statistics"))
         self.label_2.setText(_translate("MainWindow", "Message"))
         self.ClearMessage.setText(_translate("MainWindow", "Clear Message"))
         self.Close.setText(_translate("MainWindow", "Close"))
+        self.RequestData_2.setText(_translate("MainWindow", "Plot graph"))
+        self.RequestData_3.setText(_translate("MainWindow", "Test Protocols"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
-
-
 
     def fetch_data(self):
         # Receive message from SQS queue
@@ -167,42 +183,44 @@ class Ui_MainWindow(object):
 
 
             self.MessageBox.setText("Fetched Data:\n"  + self.final_mesg + "\nTimestamp: " + str(datetime.datetime.now()))
-            self.plotGraph()
-            print("\nCoAP Data:\n")
-            coapthread = threading.Thread(target=self.CoAPhandler)
-            coap_t1 = time.time()
-            coapthread.start()
-            coapthread.join()
-            coap_t2 = time.time()
-            coap_exec_time = (coap_t2 - coap_t1)
-            print('\nCoAP Protocol data exchange time: %s'% coap_exec_time)
-            print("\n\nMQTT Data:\n")
-            mqtt_t1 = time.time()
-            client.publish(up_topic, self.final_mesg)
-            msg_event.wait()
-            mqtt_t2 = time.time()
-            mqtt_exec_time = (mqtt_t2 - mqtt_t1)
-            print('\nMQTT Protocol data exchange time: %s seconds'% mqtt_exec_time)
-            msg_event.clear()
-            web_t1 = time.time()
-            self.websocket_client()
-            web_t2 = time.time()
-            web_exec_time = web_t2 - web_t1
-            print('WebSocket Protocol data exchange time: %s'% web_exec_time)
-            rabbit_t1 = time.time()
-            self.rabbitmq_publish()
-            rabbit_event.wait()
-            rabbit_t2 = time.time()
-            rabbit_exec_time = rabbit_t2 - rabbit_t1
-            print('Rabbit AMQP Protocol data exchange time: %s'% rabbit_exec_time)
-            rabbit_event.clear()
         # Error Handling
         else:
             self.MessageBox.setText("Error Fetching Data \n")
 
+    def test_protocols(self):
+        self.fetch_data()
+        print("\nCoAP Data:\n")
+        coapthread = threading.Thread(target=self.CoAPhandler)
+        coap_t1 = time.time()
+        coapthread.start()
+        coapthread.join()
+        coap_t2 = time.time()
+        coap_exec_time = (coap_t2 - coap_t1)
+        print('\nCoAP Protocol data exchange time: %s'% coap_exec_time)
+        print("\n\nMQTT Data:\n")
+        mqtt_t1 = time.time()
+        client.publish(up_topic, self.final_mesg)
+        msg_event.wait()
+        mqtt_t2 = time.time()
+        mqtt_exec_time = (mqtt_t2 - mqtt_t1)
+        print('\nMQTT Protocol data exchange time: %s seconds'% mqtt_exec_time)
+        msg_event.clear()
+        web_t1 = time.time()
+        self.websocket_client()
+        web_t2 = time.time()
+        web_exec_time = web_t2 - web_t1
+        print('WebSocket Protocol data exchange time: %s'% web_exec_time)
+        rabbit_t1 = time.time()
+        self.rabbitmq_publish()
+        rabbit_event.wait()
+        rabbit_t2 = time.time()
+        rabbit_exec_time = rabbit_t2 - rabbit_t1
+        print('Rabbit AMQP Protocol data exchange time: %s'% rabbit_exec_time)
+        rabbit_event.clear()
 
     # Function for plotting graph of humidity and temperature separately
     def plotGraph(self):
+        self.fetch_data()
         plt.plot(range(self.num_readings), self.max_temp_list, 'b-', label='Max Temp')
         plt.plot(range(self.num_readings), self.min_temp_list, 'r-', label='Min Temp')
         plt.plot(range(self.num_readings), self.curr_temp_list, 'y-', label='Last Temp')
@@ -227,13 +245,13 @@ class Ui_MainWindow(object):
     def cel_to_fah(self):
         self.mult_factor = 1.8
         self.add_factor = 32.0
-        self.unit = " F\n"
+        self.unit = ' \u00b0' + " F\n"
 
 
     def fah_to_cel(self):
         self.mult_factor = 1.0
         self.add_factor = 0.0
-        self.unit = " C\n"
+        self.unit = ' \u00b0' + " C\n"
 
     async def coapPUT(self, data):
         context = await Context.create_client_context()
@@ -291,8 +309,6 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(str(msg.payload))
     msg_event.set()
-
-
 
 
 if __name__ == "__main__":
